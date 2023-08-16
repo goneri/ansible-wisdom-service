94091ed41bd6393c40e8e9623e02cde863214714
Sumit Jaiswal <sjaiswal@redhat.com>
2023-08-17 01:07:21 +0530

    fix review
    Signed-off-by: Sumit Jaiswal <sjaiswal@redhat.com>

--- ansible_wisdom/ai/apps.py
+++ ansible_wisdom/ai/apps.py
@@ -131,14 +131,14 @@ class AiConfig(AppConfig):
         return self._wca_secret_manager
 
     def get_ansible_lint_caller(self):
+        if self._ansible_lint_caller:
+            return self._ansible_lint_caller
         if not settings.ENABLE_ANSIBLE_LINT_POSTPROCESS:
             logger.info("Ansible Lint Postprocessing is disabled.")
             self._ansible_lint_caller = UNINITIALIZED
             return None
         if self._ansible_lint_caller is FAILED:
             return None
-        if self._ansible_lint_caller:
-            return self._ansible_lint_caller
         try:
             self._ansible_lint_caller = lintpostprocessing.AnsibleLintCaller()
             logger.info("Ansible Lint Postprocessing is enabled.")
